2d0728e61353a685490c43d0a7889aa3c92de73d
Manisha Shekhawat <manishashekhawat.it>
2023-07-16 12:04:39 +0530

    2nd question

---
+++ squareroot/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        try
+        {
+            Console.WriteLine("Enter a number:");
+            double number = double.Parse(Console.ReadLine());
+
+            if (number < 0)
+            {
+                throw new Exception("Cannot calculate square root of a negative number.");
+            }
+
+            double squareRoot = Math.Sqrt(number);
+            Console.WriteLine("Square root of {0} is {1}", number, squareRoot);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Error: " + ex.Message);
+        }
+    }
+}
